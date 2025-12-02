a0e4821a2f3c7de75504ef935d85a05573735d22
Muneer Hamdan <muneerhamdan01@gmail.com>
2025-12-02 18:57:40 -0500

    asdf

--- game/soeasy/soeasy.js
+++ game/soeasy/soeasy.js
@@ -1,6 +1,14 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+
+
 let ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
